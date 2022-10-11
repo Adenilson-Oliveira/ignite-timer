3404dd14ccbb4ed9785389ca7ee9cf8929d31166
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2022-10-10 21:34:07 -0300

    add component Header and Layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from './styles'
+import { Scroll, Timer } from 'phosphor-react'
+import LogoIgnite from '../../assets/LogoIgnite.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={LogoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer" end>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
